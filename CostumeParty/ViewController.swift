7761438d917ab72a411a09e98331a425e021465d
Lane Faison <lane.faison@icloud.com>
2017-12-16 20:56:13 -0700

    Initial Commit

---
+++ CostumeParty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CostumeParty
+//
+//  Created by Lane Faison on 12/16/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
