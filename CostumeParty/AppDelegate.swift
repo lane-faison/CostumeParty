a23f914bfff0372ae78930c52fffbf1827aafee4
Lane Faison <lane.faison@icloud.com>
2018-01-08 19:52:41 -0700

    Navigation bar colors set inside the AppDelegate

--- CostumeParty/AppDelegate.swift
+++ CostumeParty/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        UINavigationBar.appearance().barTintColor = UIColor.primaryDarkColor
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.primaryTextColor]
+        UIBarButtonItem.appearance().tintColor = UIColor.primaryTextColor
+        
         return true
     }
 
