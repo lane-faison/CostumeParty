80f178ea0ef7d424aada21e5600883009598bdee
Lane Faison <lane.faison@icloud.com>
2018-01-09 00:10:05 -0700

    Code simplifying

--- CostumeParty/Helpers/FirebaseAuthHelper.swift
+++ CostumeParty/Helpers/FirebaseAuthHelper.swift
@@ -2,18 +2,22 @@ import UIKit
 import Firebase
 
 public struct FirebaseAuthHelper {
+    
+    private static let FireDatabase = Database.database().reference()
+    private static let FireAuth = Auth.auth()
+    
     public static func createUser(viewController: UIViewController, user: User, password: String, party: Party? = nil) {
-        Auth.auth().createUser(withEmail: user.email, password: password) { (firebaseUser, error) in
+        FireAuth.createUser(withEmail: user.email, password: password) { (firebaseUser, error) in
             
             if error != nil {
                 let message = "There was an error when creating your account. Please try again."
                 AlertHelper.fireErrorActionSheet(viewController: viewController, message: message)
             } else {
                 guard let firebaseUser = firebaseUser else { return }
-                Database.database().reference().child("users").child(firebaseUser.uid).child("email").setValue(firebaseUser.email)
-                Database.database().reference().child("users").child(firebaseUser.uid).updateChildValues(["costume": user.costume, "userType": user.userType.rawValue])
+                FireDatabase.child("users").child(firebaseUser.uid).child("email").setValue(firebaseUser.email)
+                FireDatabase.child("users").child(firebaseUser.uid).updateChildValues(["costume": user.costume, "userType": user.userType.rawValue])
                 if let party = party, user.userType == .host {
-                    Database.database().reference().child("parties").child(firebaseUser.uid).updateChildValues(["name": party.name, "zipCode": party.zipCode])
+                    FireDatabase.child("parties").child(firebaseUser.uid).updateChildValues(["name": party.name, "zipCode": party.zipCode])
                 }
                 let message = "Congratulations! Your account has been successfully created. Please log in to continue!"
                 AlertHelper.fireSuccessActionSheet(viewController: viewController, message: message)
@@ -23,7 +27,7 @@ public struct FirebaseAuthHelper {
     
     public static func logout(viewController: UIViewController, user: User) {
         do {
-            try Auth.auth().signOut()
+            try FireAuth.signOut()
             viewController.navigationController?.popToRootViewController(animated: true)
         } catch let error {
             AlertHelper.fireErrorActionSheet(viewController: viewController, message: error.localizedDescription)
